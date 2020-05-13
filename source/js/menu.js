d0b11af4bdfef6ac53111b66eb79094599e97f08
Venera <coldlives@gmail.com>
2020-05-13 18:10:34 +0300

    Передеала js меню

---
+++ source/js/menu.js
@@ -0,0 +1,17 @@
+var menuBtn = document.querySelector(".main-nav__btn");
+var menuItem = document.querySelectorAll(".main-nav__list--category, .main-nav__list--user");
+
+
+menuBtn.classList.toggle("main-nav__btn--open");
+for (var i = 0; i < menuItem.length; i++) {
+  menuItem[i].classList.toggle("main-nav__list--hidden")
+};
+
+
+menuBtn.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  menuBtn.classList.toggle("main-nav__btn--open");
+  for (var i = 0; i < menuItem.length; i++) {
+    menuItem[i].classList.toggle("main-nav__list--hidden")
+  };
+});
